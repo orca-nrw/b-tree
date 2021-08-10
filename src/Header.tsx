dd0b4869d62e8bdf2800a2040cf6debffd8eb224
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-08-10 19:05:37 +0200

    moved images out of public/img into correct folders

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
+import edbHeaderLogo from './img/edb_header_logo.png'
+import eildHeaderLogo from './img/eild_header_logo.png'
 
 export const Header = () => {
   return (
     <header>
       <div className='header-image-container'>
-        <img className='header-image' src="/img/edb_header_logo.png" alt="EDB Logo" />
-        <img className='header-image' src="/img/eild_header_logo.png" alt="EILD Logo" />
+        <img className='header-image' src={edbHeaderLogo} alt="EDB Logo" />
+        <img className='header-image' src={eildHeaderLogo} alt="EILD Logo" />
       </div>
     </header>
   )
