3f3cd93f5000bb58f3f10efe6f2c3391143f64e1
Alexander Kosmehl <alexander.kosmehl@th-koeln.de>
2021-08-15 18:55:50 +0200

    Moved drawing logic into canvasHelper

--- src/Helper/canvasNode.ts
+++ src/Helper/canvasNode.ts
@@ -10,13 +10,4 @@ export class CanvasNode {
     this.width = 55
     this.height = 24
   }
-
-  insertValues (text: string, context: CanvasRenderingContext2D) {
-    context.fillStyle = 'rgba(0, 0, 0, 1)'
-    const xText = this.x + this.width / 2 - context.measureText(text).width / 2
-    const lineHeight = context.measureText('M').width
-    const yText = this.y + this.height / 2 + lineHeight / 2
-
-    context.fillText(text, xText, yText)
-  }
 }
