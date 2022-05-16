50b0d2d5270091d8ff96043b2fb24cd04b778a5c
Alexander Kosmehl <kosmehl@live.de>
2022-05-16 16:02:09 +0200

    Added eild link

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -7,10 +7,20 @@ export const Header = () => {
     <header className="bg-white p-4">
       <div className="w-full flex">
         <div className="flex-auto">
-          <img className="max-h-32 max-w-full" src={edbHeaderLogo} alt="EDB Logo" />
+          <img
+            className="max-h-32 max-w-full"
+            src={edbHeaderLogo}
+            alt="EDB Logo"
+          />
         </div>
         <div className="flex-auto">
-          <img className="max-h-32 max-w-full float-right" src={eildHeaderLogo} alt="EILD Logo" />
+          <a href="https://medien.hs-duesseldorf.de/eild">
+            <img
+              className="max-h-32 max-w-full float-right"
+              src={eildHeaderLogo}
+              alt="EILD Logo"
+            />
+          </a>
         </div>
       </div>
     </header>
